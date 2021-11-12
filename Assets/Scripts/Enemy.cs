f7086b0c1f2fdd32eccc686ddeacc9591132c883
seif565 <assassin56731@gmail.com>
2021-11-12 14:45:59 +0200

    2D Platformer Scripts
    Includes Player, Enemy, Game Settings and Game Event Handler.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Parameters
+    [SerializeField] float enemyMoveSpeed = 5.0f;
+    [SerializeField] float waitTime = 2f;    
+    [SerializeField] Transform castPoint;    
+    float currentMoveSpeed;
+    float movementDirection;
+    
+
+    // Cahced References
+    [SerializeField] BoxCollider2D bodyCollider, terrainCollider;
+    Rigidbody2D enemyRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        currentMoveSpeed = enemyMoveSpeed;
+        enemyRB = GetComponent<Rigidbody2D>();
+        movementDirection = transform.localScale.x;
+    }
+
+    void Update()
+    {
+        //if (FindObjectsOfType<PlayerMovement>().Length != 0)
+        //{
+        //    float distance = Vector2.Distance(transform.position, FindObjectOfType<PlayerMovement>().transform.position);
+        //    Debug.Log(canSeePlayer(distance));
+            
+        //}
+    }
+
+    void FixedUpdate()
+    {        
+        enemyRB.velocity = new Vector2(movementDirection * currentMoveSpeed, enemyRB.velocity.y);
+    }
+    
+    /**bool canSeePlayer(float distance)
+    {
+        bool isVisible = false;
+        float castDist = distance;
+        Vector2 endPos = castPoint.position + Vector3.right * distance; // 
+        RaycastHit2D hit = Physics2D.Linecast(castPoint.position, endPos, 1 << LayerMask.NameToLayer("Player"));
+        if(hit.collider == null)
+        {
+            Debug.Log("err");
+            isVisible = true;
+        }
+        return isVisible;
+    }**/
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 3)
+        {
+            Debug.Log("targer hit.");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StartCoroutine(WaitAndTurn());
+    }
+    private IEnumerator WaitAndTurn()
+    {
+        currentMoveSpeed = 0;
+        yield return new WaitForSeconds(waitTime);
+        currentMoveSpeed = enemyMoveSpeed;
+        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        movementDirection = transform.localScale.x;        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement :  MonoBehaviour
+{
+
+    // Properties
+    [Header("Movement Attributes")]    
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float attackCD = 2f;    
+    [SerializeField] float dodgeForce = 500f;
+
+    [Header("Timers")]
+    [SerializeField] float dodgeCD = 0.3f;
+    [SerializeField] float invincibilityTime = 0.3f;
+
+    [Header("Serialized Components")]
+    [SerializeField] BoxCollider2D swordHitBox;
+    
+    float movementDirection;
+    float timeToAttack;
+    float timeToDodge;
+    bool isGrounded = true;
+
+    // References
+    Rigidbody2D playerRB;
+    CapsuleCollider2D playerCollider;
+    Animator playerAnimator;        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
+        playerAnimator = GetComponent<Animator>();
+        timeToAttack = 1f;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (timeToAttack > 0) { timeToAttack -= Time.deltaTime; }
+
+        if (timeToDodge > 0) { timeToDodge -= Time.deltaTime; }
+
+        movementDirection = Input.GetAxisRaw("Horizontal");
+        isGrounded = playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        Debug.Log(isGrounded);
+        // Animation state check
+        playerAnimator.SetBool("isGrounded", isGrounded);
+        playerAnimator.SetBool("isRunning", movementDirection!= 0 && isGrounded);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerRB.AddForce(new Vector2(0, jumpForce));
+        }
+
+        if (movementDirection != 0) { transform.localScale = new Vector3(movementDirection, 1, 1); }
+
+        if (Input.GetButtonDown("Attack") && timeToAttack <= 0)
+        {
+            StartCoroutine(Attack());
+        }
+
+        //if (Input.GetKeyDown(KeyCode.F)){
+        //    Time.timeScale = 0;
+        //}
+
+
+        if (Input.GetButtonDown("Evade") && timeToDodge <= 0)
+        {
+            StartCoroutine(Dodge());
+        }        
+    }
+
+    public IEnumerator Dodge()
+    {
+        gameObject.layer = 8;
+        playerRB.AddForce(new Vector2(dodgeForce * transform.localScale.x, 0));
+        playerAnimator.SetTrigger("Dodge");        
+        timeToDodge = dodgeCD;
+        yield return new WaitForSeconds(invincibilityTime);
+        gameObject.layer = 3;
+    }
+   
+ 
+
+    public IEnumerator Attack()
+    {
+        playerAnimator.SetTrigger("Attack");
+        timeToAttack = attackCD;
+        swordHitBox.enabled = true;
+        Debug.Log("enabled");
+        yield return new WaitForSeconds(0.15f);
+        Debug.Log("disabled");
+        swordHitBox.enabled= false;
+    }
+
+    private void FixedUpdate()
+    {
+
+        playerRB.velocity = new Vector2(moveSpeed * movementDirection, playerRB.velocity.y);        
+        //Debug.Log(playerRB.velocity.x);
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Player Collision Detection
+
+        // Sword Hit Detection
+        if (swordHitBox.IsTouching(collision))
+        {            
+            if(collision.gameObject.layer == 7)
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        Debug.Log("Player Hit Something");
+        if (collision.gameObject.layer == 7)
+        {
+            Destroy(gameObject);
+            Debug.Log("momo");
+        }
+    }
+}
