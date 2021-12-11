3ae1be793092b10fc5d483930679491dff031554
seif565 <assassin56731@gmail.com>
2021-12-11 23:32:17 +0200

    BACKUP

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,18 +1,23 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHandler : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] TextMeshProUGUI livesText;
     [SerializeField] TextMeshProUGUI gameOverText;
+    [SerializeField] Button playAgainButton;
+    [SerializeField] Button mainMenuButton;
     LevelHandler levelHandler;
     // Start is called before the first frame update
     void Start()
     {
         levelHandler = FindObjectOfType<LevelHandler>();
-        gameOverText.gameObject.SetActive(false);        
+        gameOverText.gameObject.SetActive(false);
+        playAgainButton.gameObject.SetActive(false);
         scoreText.SetText(levelHandler.GetScore().ToString());
+        mainMenuButton.gameObject.SetActive(false);        
     }
     
     public void UpdateScore()
@@ -25,7 +30,9 @@ public class UIHandler : MonoBehaviour
     }
 
     public void ShowGameOverUI()
-    {
+    {        
         gameOverText.gameObject.SetActive(true);
+        playAgainButton.gameObject.SetActive(true);
+        mainMenuButton.gameObject.SetActive(true);
     }
 }
